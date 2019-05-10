0dd1ce13ec3f393d2531de0ce9535a2512281d98
Hesham <heshamsaid@hotmail.com>
2019-05-10 08:55:09 +0200

    Handled possible NullPointerException

--- app/src/main/java/com/example/qrcodescannerdemo/MainActivity.java
+++ app/src/main/java/com/example/qrcodescannerdemo/MainActivity.java
@@ -103,6 +103,8 @@ public class MainActivity extends AppCompatActivity {
             else
             {
                 try {
+                    if (data.getData() == null)
+                        throw new Exception();
                     InputStream inputStream = getContentResolver().openInputStream(data.getData());
                     bitmap = BitmapFactory.decodeStream(inputStream);
                     barCodeImageView.setImageBitmap(bitmap);
